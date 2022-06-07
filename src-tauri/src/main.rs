5c09bec18a985a007f4982f4336718da81fecd9b
Dany Santos <theorlan2@hotmail.es>
2022-06-07 11:49:53 -0400

    new logo, fix error Patch env, etc...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,7 @@ fn check_is_port_run(port: u16) ->Result<bool,bool> {
 }
 
 fn main() {
+  fix_path_env::fix();
   tauri::Builder::default()
   .invoke_handler(tauri::generate_handler![check_is_port_run])  
   .run(tauri::generate_context!())
--- src/components/Layout/Drawer.tsx
+++ src/components/Layout/Drawer.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
-import logo from '../../logo.svg'
+import logo from '../../assets/logo.png'
+import logoWhite from '../../assets/logo_white.png'
 
 type Props = {
     packageActive: boolean;
@@ -17,23 +18,24 @@ const Drawer: FunctionComponent<Props> = (props) => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                 </button> */}
-                <button className='rounded-full bg-slate-500 text-white p-1 hover:bg-slate-600 dark:bg-gray-800 '  ><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <button className='rounded-full bg-slate-500 text-white p-1 hover:bg-slate-600 dark:bg-gray-800 hover:dark:bg-slate-900 '  ><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                 </svg></button>
             </div>
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo block dark:hidden m-auto" alt="logo" />
+            <img src={logoWhite} className="App-logo hidden dark:block m-auto" alt="logo" />
             {props.packageActive && <div className="cont-package-name text-left mb-2">
                 <h3 className='text-base text-gray-500 dark:text-white' >Package: <p className='package-name text-xs dark:text-white font-bold' >{props.packageName}</p></h3>
             </div>}
             <p className='text-title text-gray-500 dark:text-white text-lg uppercase font-medium' >Android ADB Commands</p>
             <div className="cont_buttons">
-                <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white' onClick={() => { props.action('listPackets') }}>
+                <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white hover:dark:bg-slate-900 ' onClick={() => { props.action('listPackets') }}>
                     LISTA DE PAQUETES
                 </button>
-                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white' onClick={() => { props.action('startApp'); }}>INICIAR APLICACION</button>}
-                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white' onClick={() => { props.action('stopApp'); }}>DENTENER</button>}
-                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white' onClick={() => { props.action('clean'); }}>LIMPIAR DATOS</button>}
-                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white' onClick={() => { props.action('cleanAndRestart'); }}>LIMPIAR DATOS Y REINICIAR</button>}
+                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white hover:dark:bg-slate-900' onClick={() => { props.action('startApp'); }}>INICIAR APLICACION</button>}
+                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white hover:dark:bg-slate-900' onClick={() => { props.action('stopApp'); }}>DENTENER</button>}
+                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white hover:dark:bg-slate-900' onClick={() => { props.action('clean'); }}>LIMPIAR DATOS</button>}
+                {props.packageActive && <button type="button" className='my-2 d-block w-full  h-10 px-6 font-semibold text-sm rounded transition hover:bg-slate-600 bg-slate-500 dark:bg-gray-800 text-white hover:dark:bg-slate-900' onClick={() => { props.action('cleanAndRestart'); }}>LIMPIAR DATOS Y REINICIAR</button>}
             </div>
         </header>
     )
